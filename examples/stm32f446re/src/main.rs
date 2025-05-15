841125546a9c7eed4759d4ab986142b909c1dbae
erichstuder <richu_@hotmail.com>
2025-05-15 17:53:00 +0200

    async state machine example

--- examples/stm32f446re/src/main.rs
+++ examples/stm32f446re/src/main.rs
@@ -6,8 +6,12 @@
 use defmt::*;
 use embassy_executor::{Spawner, main, task};
 use embassy_stm32::gpio::{Level, Output, Speed};
+// use embassy_stm32::usart::{Config, Uart};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+
+mod state_machine;
+use state_machine::{StateMachine, StateActions};
 
 #[main]
 async fn main(spawner: Spawner) {
@@ -17,6 +21,16 @@ async fn main(spawner: Spawner) {
     let led = Output::new(peripherals.PA5, Level::High, Speed::Low);
 
     spawner.spawn(blink(led)).unwrap();
+
+    struct StateActionsImpl;
+    impl StateActions for StateActionsImpl {
+        async fn wait_ms(&self, duration: u64) {
+            Timer::after_millis(duration).await;
+        }
+    }
+
+    let mut state_machine = StateMachine::new(&StateActionsImpl);
+    state_machine.run().await;
 }
 
 #[task]
