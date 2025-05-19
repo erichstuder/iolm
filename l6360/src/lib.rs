45fbcdce3913b3c3181219fd2440534bb9969aee
erichstuder <richu_@hotmail.com>
2025-05-19 19:48:30 +0200

    use mock!

--- l6360/src/lib.rs
+++ l6360/src/lib.rs
@@ -69,57 +69,29 @@ impl<I2cType: I2c> L6360<I2cType> {
 #[cfg(test)]
 mod tests {
     use super::{L6360, Led};
-    use embedded_hal_async::i2c::{self, I2c};
-    use std::cell::RefCell;
-    use std::rc::Rc;
+    use embedded_hal_async::i2c;
     use tokio;
+    use mockall::*;
 
+    mock! {
+        pub I2c {}
 
-    #[derive(Default)]
-    struct I2cMockState {
-        pub i2c_address: [i2c::SevenBitAddress; 2],
-        pub write_len: [usize; 2],
-        pub write_byte0: [u8; 2],
-        pub write_byte1: [u8; 2],
-        pub write_call_cnt: u8,
-    }
-    struct I2cMock {
-        pub state: Rc<RefCell<I2cMockState>>,
-    }
-
-    impl Default for I2cMock {
-        fn default() -> Self {
-            Self {
-                state: Rc::new(RefCell::new(I2cMockState::default())),
-            }
-        }
-    }
-
-    impl i2c::ErrorType for I2cMock {
-        type Error = core::convert::Infallible;
-    }
-
-    impl I2c for I2cMock {
-        async fn write(&mut self, address: i2c::SevenBitAddress, write: &[u8]) -> Result<(), Self::Error> {
-            let mut state = self.state.borrow_mut();
-            let index = state.write_call_cnt as usize;
-            state.i2c_address[index] = address;
-            state.write_len[index] = write.len();
-            state.write_byte0[index] = write[0];
-            state.write_byte1[index]  = write[1];
-            state.write_call_cnt += 1;
-            Ok(())
+        impl i2c::ErrorType for I2c {
+            type Error = core::convert::Infallible;
         }
 
-        async fn transaction(&mut self, _address: i2c::SevenBitAddress, _operations: &mut [i2c::Operation<'_>]) -> Result<(), Self::Error> {
-            panic!("We should never come here in a test as write and read should be mocked.");
+        impl i2c::I2c for I2c {
+            // async fn read(&mut self, address: i2c::SevenBitAddress, buffer: &mut [u8]) -> Result<(), <Self as i2c::ErrorType>::Error>;
+            async fn write(&mut self, address: i2c::SevenBitAddress, bytes: &[u8]) -> Result<(), <Self as i2c::ErrorType>::Error>;
+            // async fn write_read(&mut self, address: i2c::SevenBitAddress, bytes: &[u8], buffer: &mut [u8]) -> Result<(), <Self as i2c::ErrorType>::Error>;
+            async fn transaction<'a>(&mut self, address: i2c::SevenBitAddress, operations: &mut [i2c::Operation<'a>]) -> Result<(), <Self as i2c::ErrorType>::Error>;
         }
     }
 
     #[tokio::test]
     async fn test_new() {
         for address in 0..=255 {
-            let i2c_mock = I2cMock::default();
+            let i2c_mock = MockI2c::new();
             let result = L6360::new(i2c_mock, address);
             if address < 0b0_1100_000 || address > 0b0_1100_111 {
                 assert!(result.is_err(), "L6360::new returned ok, with address: {:?}", address);
@@ -132,14 +104,14 @@ mod tests {
 
     #[tokio::test]
     async fn test_set_led_pattern() {
-        let test_cases:
-                //input                           //expect
-            &[( u8,           Led,       u16,     u8,            u8,            u8,            u8 )] = &[
-            //  i2c_address,  led,       pattern, write_byte_00, write_byte_10, write_byte_01, write_byte_11
-            (   0b0_1100_101, Led::LED1, 0x0000,  0x00,          0x04,          0x00,          0x05, ),
-            (   0b0_1100_111, Led::LED2, 0x0000,  0x00,          0x06,          0x00,          0x07, ),
-            (   0b0_1100_000, Led::LED1, 0x55AA,  0x55,          0x04,          0xAA,          0x05, ),
-            (   0b0_1100_001, Led::LED2, 0x1234,  0x12,          0xC6,          0x34,          0x67, ),
+        let test_cases: &[(
+               // input                          // expect
+               u8,           Led,       u16,     u8,            u8,            u8,            u8 )] = &[
+            // i2c_address,  led,       pattern, write_byte_00, write_byte_10, write_byte_01, write_byte_11
+            (  0b0_1100_101, Led::LED1, 0x0000,  0x00,          0x04,          0x00,          0x05, ),
+            (  0b0_1100_111, Led::LED2, 0x0000,  0x00,          0x06,          0x00,          0x07, ),
+            (  0b0_1100_000, Led::LED1, 0x55AA,  0x55,          0x04,          0xAA,          0x05, ),
+            (  0b0_1100_001, Led::LED2, 0x1234,  0x12,          0xC6,          0x34,          0x67, ),
 
         ];
 
@@ -148,27 +120,32 @@ mod tests {
             println!("test_cnt: {:?}", test_cnt);
             test_cnt += 1;
 
-            let i2c_mock_state = Rc::new(RefCell::new(I2cMockState {
-                write_call_cnt: 0,
-                ..Default::default()
-            }));
-            let i2c_mock = I2cMock {
-                state: i2c_mock_state.clone(),
-            };
+            let mut i2c_mock = MockI2c::new();
+
+            i2c_mock
+                .expect_write()
+                .times(1)
+                .withf(move |address, bytes| {
+                    *address == *i2c_address &&
+                    bytes.len() == 2 &&
+                    bytes[0] == *write_byte_00 &&
+                    bytes[1] == *write_byte_10
+                })
+                .returning(|_, _| Ok(()));
+
+            i2c_mock
+                .expect_write()
+                .times(1)
+                .withf(move |address, bytes| {
+                    *address == *i2c_address &&
+                    bytes.len() == 2 &&
+                    bytes[0] == *write_byte_01 &&
+                    bytes[1] == *write_byte_11
+                })
+                .returning(|_, _| Ok(()));
+
             let mut l63601 = L6360::new(i2c_mock, *i2c_address).unwrap();
             l63601.set_led_pattern(*led, *pattern).await.unwrap();
-
-            let i2c_mock_state = i2c_mock_state.borrow();
-            assert_eq!(i2c_mock_state.i2c_address[0], *i2c_address);
-
-            assert_eq!(i2c_mock_state.write_len[0],   2);
-            assert_eq!(i2c_mock_state.write_byte0[0], *write_byte_00);
-            assert_eq!(i2c_mock_state.write_byte1[0], *write_byte_10);
-            assert_eq!(i2c_mock_state.i2c_address[1], *i2c_address);
-            assert_eq!(i2c_mock_state.write_len[1],   2);
-            assert_eq!(i2c_mock_state.write_byte0[1], *write_byte_01);
-            assert_eq!(i2c_mock_state.write_byte1[1], *write_byte_11);
-            assert_eq!(i2c_mock_state.write_call_cnt, 2);
         }
     }
 
@@ -199,7 +176,7 @@ mod tests {
         println!("|:----------:|:--------:|");
         for (data, expected) in test_cases {
             println!("| 0b{:08b} |   0b{:03b}  |", data, expected);
-            assert_eq!(L6360::<I2cMock>::calculate_parity(*data), *expected);
+            assert_eq!(L6360::<MockI2c>::calculate_parity(*data), *expected);
         }
     }
 }
