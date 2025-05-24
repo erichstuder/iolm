d47a76ff52673ed6296e7c5fb33eb98c9446228c
erichstuder <richu_@hotmail.com>
2025-05-24 15:30:30 +0200

    make state machine less chatty

--- examples/stm32f446re/src/state_machine.rs
+++ examples/stm32f446re/src/state_machine.rs
@@ -24,6 +24,7 @@ impl<'a, T: StateActions> StateMachine<'a, T> {
     }
 
     pub async fn run(&mut self) {
+        info!("run state machine");
         loop {
             self.next().await;
         }
@@ -32,17 +33,17 @@ impl<'a, T: StateActions> StateMachine<'a, T> {
     async fn next(&mut self) {
         match self.state {
             State::State1 => {
-                info!("State 1");
+                //info!("State 1");
                 self.state_actions.wait_ms(777).await;
                 self.state = State::State2;
             }
             State::State2 => {
-                info!("State 2");
+                //info!("State 2");
                 self.state_actions.wait_ms(2222).await;
                 self.state = State::State3;
             }
             State::State3 => {
-                info!("State 3");
+                //info!("State 3");
                 self.state_actions.wait_ms(123).await;
                 self.state = State::State1;
             }
