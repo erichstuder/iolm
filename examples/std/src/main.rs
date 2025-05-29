199bc8f85d266665ff0a473532f04271221c44a3
erichstuder <richu_@hotmail.com>
2025-05-29 14:48:26 +0200

    setup env_logger

--- examples/std/src/main.rs
+++ examples/std/src/main.rs
@@ -6,22 +6,43 @@ use iol::master_dl;
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
-
-    info!("Hello!!!!");
+    setup_logger();
 
     let (mut dl, dl_mode_handler) = master_dl::DL::new();
     tokio::spawn(run_dl(dl_mode_handler));
 
     sleep(Duration::from_secs(2)).await;
 
-    println!("sending signal");
+    info!("sending signal");
     dl.DL_SetMode(master_dl::Mode::STARTUP).await.unwrap();
 
-    sleep(Duration::from_secs(100)).await;
+    sleep(Duration::from_secs(3)).await;
 }
 
 async fn run_dl(mut dl: master_dl::StateMachine<master_dl::StateActionsImpl>) {
-    println!("run dl");
+    info!("run dl");
     dl.run().await;
+}
+
+fn setup_logger() {
+    env_logger::Builder::from_default_env()
+    .format(|buf, record| {
+        use std::io::Write;
+        use chrono::Local;
+        use anstyle;
+
+        writeln!(
+            buf,
+            "{} {}{}{}\t{}\n  \x1b[2m{} @ {}:{}\x1b[0m",
+            Local::now().format("%Y-%m-%d %H:%M:%S%.6f"),
+            buf.default_level_style(record.level()),
+            record.level(),
+            anstyle::Reset,
+            record.args(),
+            record.module_path().unwrap_or("unknwon module"),
+            record.file().unwrap_or("unknown file"),
+            record.line().unwrap_or(0),
+        )
+    })
+    .init();
 }
