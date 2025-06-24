211a54642a832fdea9cf410b56db265b80e02885
erichstuder <richu_@hotmail.com>
2025-06-24 18:43:27 +0200

    move software to its own folder

---
+++ software/run.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sys
+import pathlib
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent / 'project_management'))
+from executor import Executor # type: ignore
+
+if __name__ == "__main__":
+    additional_arguments = [
+        {
+            'flag': '-b',
+            'name': '--build',
+            'help': 'Build the software.'
+        },
+        {
+            'flag': '-t',
+            'name': '--test',
+            'help': 'Test the software.'
+        },
+        {
+            'flag': '-d',
+            'name': '--doc',
+            'help': 'Document the software.'
+        }
+    ]
+
+    ex = Executor(additional_arguments, description='Execute feature tests')
+
+    if ex.arguments.build:
+        commands = (
+            'cd examples/stm32f446re && cargo build && cd - &&'
+            'cd examples/std && cargo build && cd -'
+        )
+    elif ex.arguments.test:
+        commands = (
+            'cargo test --manifest-path l6360/Cargo.toml &&'
+            # Note: log feature is not specifically tested and always enabled so it compiles.
+            'cargo test --manifest-path iol/Cargo.toml --features "log master" &&'
+            'cargo test --manifest-path iol/Cargo.toml --features "log master iols"'
+        )
+    elif ex.arguments.doc:
+        commands = (
+            'cargo doc --manifest-path l6360/Cargo.toml --no-deps &&'
+            'cargo doc --manifest-path iol/Cargo.toml --no-deps '
+        )
+    else:
+        commands = None
+
+    ex.run(commands)
